fd367173770852584c761b5cc4b38867dd39388d
Jonathan Rampersad <c1ph3r.dev@gmail.com>
2023-09-11 18:56:37 -0400

    code refactor

--- include/utils.h
+++ include/utils.h
@@ -10,14 +10,5 @@ namespace EM6502
     using s32 = signed int;
 
     static constexpr u32 MAX_MEM = 1024 * 64;
-
-    enum class opcodes : Byte
-    {
-        INS_NOP = 0xEA,
-        INS_LDA_IM = 0xA9,
-        INS_LDA_ZP = 0xA5,
-        INS_LDA_ZPX = 0xB5,
-        INS_JSR = 0x20
-    };
 }
 #endif // EM6502_UTILS_H
